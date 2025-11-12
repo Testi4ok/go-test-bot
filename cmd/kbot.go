76971615af9c9aa1dc1884511afce39e6ea8e5c0
Testi4ok <maksgrabar13@gmail.com>
2025-11-12 16:39:44 +0200

    First telegram bot

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,13 +1,14 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
 )
 
 // kbotCmd represents the kbot command
@@ -15,26 +16,47 @@ var kbotCmd = &cobra.Command{
 	Use:   "kbot",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+and usage of using your command.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kbot called")
+
+		// Load .env file before reading env vars
+		if err := godotenv.Load(); err != nil {
+			log.Println("Warning: .env file not found")
+		}
+
+		// Get TELE_TOKEN from environment
+		TeleToken := os.Getenv("TELE_TOKEN")
+		if TeleToken == "" {
+			log.Fatal("TELE_TOKEN is not set in environment or .env file")
+		}
+
+		fmt.Println("kbot started", appVersion)
+
+		kbot, err := telebot.NewBot(telebot.Settings{
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatalf("Failed to create bot: %v", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm kbot %s!", appVersion))
+			}
+
+			return nil
+		})
+
+		kbot.Start()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(kbotCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
